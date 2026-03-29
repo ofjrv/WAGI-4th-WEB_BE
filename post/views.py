bf7e0761a7eee6e7153533e959df42a3d0929878
hyein <khyein@example.com>
2026-03-29 23:54:31 +0900

    권혜인 1주차 과제 제출

--- post/views.py
+++ post/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post
 
-# Create your views here.
+# 글 작성
+def write(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        image = request.FILES.get('image')
+
+        Post.objects.create(
+            title=title,
+            content=content,
+            image=image
+        )
+        return redirect('home')
+
+    return render(request, 'write.html')
+
+
+# 글 목록
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'list.html', {'posts': posts})
+
+
+# 글 상세
+def detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'detail.html', {'post': post})
