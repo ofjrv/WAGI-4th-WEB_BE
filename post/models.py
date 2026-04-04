c6aa41f15c2ed0ead19600b825d3b4dd6a4db908
leehj <leehyunji070730@naver.com>
2026-04-04 20:49:39 +0900

    3주차 과제

--- post/models.py
+++ post/models.py
@@ -4,8 +4,11 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length = 200)
     content = models.TextField()
-    image = models.ImageField(upload_to = 'images/', blank = True, null = True)
 
     def __str__(self):
         return self.title
     
+
+class Image(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
--- post/views.py
+++ post/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render, redirect
-from .models import Post
+from .models import Post, Image
 
 # Create your views here.
 def write(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         content = request.POST.get('content')
-        image = request.FILES.get('image')
-
-        Post.objects.create(
+        post = Post.objects.create(
             title = title, 
-            content = content, 
-            image = image
+            content = content
         )
+        images = request.FILES.getlist('images')
+
+        for img in images:
+            Image.objects.create(
+                post = post,
+                image = img
+            )
+        
         return redirect('home')
     
     return render(request, 'write.html')
@@ -24,3 +29,15 @@ def home(request):
 def detail(request, post_id):
     post = Post.objects.get(id = post_id)
     return render(request, 'detail.html', {'post' : post})
+
+def update(request, id):
+    post = Post.objects.get(id = id)
+
+    if request.method == "POST":
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+
+        return redirect(f'/detail/{post.id}/')
+    
+    return render(request, 'update.html', {'post' : post})
